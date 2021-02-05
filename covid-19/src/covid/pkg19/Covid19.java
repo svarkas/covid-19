870774c5f614ff533ca3c1de951359be266b5717
Stavros <stvarkas@gmail.com>
2021-02-05 13:40:26 +0200

    tetetet

--- covid-19/src/covid/pkg19/Covid19.java
+++ covid-19/src/covid/pkg19/Covid19.java
@@ -34,7 +34,6 @@ public class Covid19 extends JApplet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-           System.out.println("dd");
         SwingUtilities.invokeLater(new Runnable() {
          
             @Override
